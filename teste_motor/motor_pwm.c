47f0e2a5d8111299ff28398f074afc44f3de539c
Lucas Eliseu Gonçalves de Arruda <lucas.arruda@embedded.ufcg.edu.br>
2018-07-10 15:46:30 -0300

    Adiciona teste simples de PWM
    A ideia é gerar na porta 2_6 um sinal PWM com valor médio 2.5V
    (ou metade do valor total) e 5V na porta 2_7 (ou o valor de ref).

--- teste_motor/motor_pwm.c
+++ teste_motor/motor_pwm.c
@@ -21,30 +21,12 @@ void main(void) {
   configura_serial(); //Configuração da serial
   configura_PWM(); //Configuração do PWM
   while (1) {
-    //Início da aquisição de 16 valores do canal 03------------------------------------------------
-    for (i=0; i<16; i++)
-    {
-      ADCCON2 = (ADCCON2 & 0xF0) | 0x03; //Seleciona o canal 03
-      //Utilizamos o canal 3 do conv AD devido ao aproveitamento do LDR que está conectado ao mesmo.
-      SCONV = 1; //Inicia a conversão
-      while (SCONV); //Espera a conversão terminar
-      bufferad = ADCDATAL | ((ADCDATAH & 0x0F) << 8); // Valor do canal do ADC selecionado
-      temp = temp + bufferad; //o valor temporário acumula 16 conversões
-    }
-    //Fim da aquisição ---------------------------------------------------------------------------
-    valor_ad = (temp/16); //média das conversões obtidas e armazenadas na variável temp
-    temp = 0; //anula a variável temporária para acumular o próximo conjunto de conversões
-    valor_ad_conv = valor_ad*0.00061035; //Média dos valores lidos convertida para tensão (V)
-    //escalona o valor AD de 12 bits para a variável Duty de 16 bits
-    Duty = (valor_ad * 65535)/4095;
-    //desloca para direita os oito bits mais significativos para as posições dos oito bits menos
-    //significativos.
-    PWM0H = Duty >> 8;
-    // Armazena os oito bits mais significativos do Duty em PWM0H
-    PWM0L = (Duty & 0x00FF); // Armazena os oito bits menos significativos do Duty em PWM0L
-    Duty_Cycle = (Duty*100.0)/65535.0; //Valor em percentagem do Duty_Cycle
-    //Mostra valor na saida serial:
-    printf("Valor Analógico: %f Duty Cycle: %f%%\n", valor_ad_conv, Duty_Cycle);
+    //Configura o valor de referencia do PWM0 para ser 2,5V e o PWM1 para ser 5V
+    PWM0H = 0x0000;
+    PWM0L = 0xFFFF; 
+    PWM1H = 0xFFFF;
+    PWM1L = 0xFFFF; 
+    printf("Imprimindo as coisas aqui \n");
   }
 }
 
