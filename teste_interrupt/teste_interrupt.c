a4651ddd18c687bb52e5d81e2c5fce72f0a96c01
Lucas Eliseu Gonçalves de Arruda <lucas.arruda@ee.ufcg.edu.br>
2018-07-23 17:02:37 -0300

    Realiza teste de interrupção
    Pisca um LED na rotina de interrupção. Teste realizado com
    as portas 3.2 (INT0/) e 3.3 (INT1/).

--- teste_interrupt/teste_interrupt.c
+++ teste_interrupt/teste_interrupt.c
@@ -7,18 +7,30 @@ void main (void){
 	PLLCON = 0x88; //Habilita o pino de fast interrupt response com 16.7 MHz
 	configura_serial(); //Configura a comunicação serial do AduC
  	//Configura o TIC
- 	TIMECON = 0x13; // configura o TIC para contar um período único de 10 segundos
- 	INTVAL = 0x0A; // 10 segundos
- 	IEIP2 = 0x44; // habilita interrupções do TIC
+ 	//TIMECON = 0x13; // configura o TIC para contar um período único de 10 segundos
+ 	//INTVAL = 0x0A; // 10 segundos
+ 	//IEIP2 = 0x44; // habilita interrupções do TIC
+
+
+	INT0 = 1;
+	INT1 = 1;
+	TCON = 0x05;  // trigger em borda de descida
  	EA = 1; // habilita todas as interrupções
+	EX0 = 1; // habilita interrupção externa
+	PX0 = 1; // prioridade pra externa
+	EX1 = 1; // habilita interrupção externa
+	PX1 = 1; // prioridade pra externa
  	P3_4 = 0;
 	while (1){
+		if(!EA){
+			EA = 1;
+		}
  	}
 }
 
 
-void TIC_int () interrupt 10{
-	TIMECON=0x13;
+void TIC_int () interrupt 0{
+	//TIMECON=0x13;
  	P3_4 = !P3_4;
  	printf("10 segundos foram ultrapassados\n");
 }
